6299cb1e21b21ac5326ae05d2e067ce36cf5e16d
Bram van Rhee <bramvanrhee01@gmail.com>
2020-11-23 10:43:18 +0100

    Eerste persoon wordt nu goed uitgelezen

--- personen.php
+++ personen.php
@@ -1,4 +1,8 @@
 <?php
 $xml=simplexml_load_file("personen.xml") or die("Error: Cannot create object");
-print_r($xml);
+echo $xml->persoon->naam . "<br>";
+echo $xml->persoon->geslacht . "<br>";
+echo $xml->persoon->haarkleur . "<br>";
+echo $xml->persoon->leeftijd . "<br>";
+echo $xml->persoon->lichaamslengte;
 ?>
