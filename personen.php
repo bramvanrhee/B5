98a35cf5c858e9adc4c8ca60c7539f9184754e26
Bram van Rhee <bramvanrhee01@gmail.com>
2020-11-23 10:55:54 +0100

    Loop gemaakt voor alle personen

--- personen.php
+++ personen.php
@@ -1,8 +1,10 @@
 <?php
 $xml=simplexml_load_file("personen.xml") or die("Error: Cannot create object");
-echo $xml->persoon->naam . "<br>";
-echo $xml->persoon->geslacht . "<br>";
-echo $xml->persoon->haarkleur . "<br>";
-echo $xml->persoon->leeftijd . "<br>";
-echo $xml->persoon->lichaamslengte;
+foreach($xml->persoon as $persoon) {
+    echo $persoon->naam;
+    echo $persoon->geslacht;
+    echo $persoon->haarkleur;
+    echo $persoon->leeftijd;
+    echo $persoon->lichaamslengte;
+}
 ?>
